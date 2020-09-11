347ab272241992182af8e8821955075db689f4dc
denisRudie <moonstamp1964@gmail.com>
2020-09-12 01:10:24 +0300

    2.3. Загрузка деталей поста. [#321332]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.job4j.utils.DateUtils;
 
+import java.io.IOException;
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -38,16 +39,20 @@ public class SqlRuParse {
         sdf = new SimpleDateFormat("d MMMMM yy, hh:mm", dfs);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SqlRuParse parser = new SqlRuParse();
 
         String url = "https://www.sql.ru/forum/job-offers/";
         ArrayList<Elements> elements = new ArrayList<>();
         for (int i = 1; i < 6; i++) {
             StringBuilder sb = new StringBuilder().append(url).append(i);
-            Document doc = Jsoup.connect(sb.toString()).get();
-            Elements row = doc.select(".postslisttopic");
-            elements.add(row);
+            try {
+                Document doc = Jsoup.connect(sb.toString()).get();
+                Elements row = doc.select(".postslisttopic");
+                elements.add(row);
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
+            }
         }
 
         elements.stream()
@@ -59,5 +64,44 @@ public class SqlRuParse {
                     Element date = e.parent().child(5);
                     System.out.println(DateUtils.getDate(date.text(), parser.dfs, parser.sdf));
                 });
+    }
+
+    /**
+     * Method parses html page for getting post message.
+     *
+     * @param link url of page with post.
+     * @return message from post.
+     */
+    public String getMsg(String link) {
+        String msg = "";
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Element row =
+                    doc.getElementsByClass("msgTable").get(0).getElementsByClass("msgBody").get(1);
+            msg = row.text();
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return msg;
+    }
+
+    /**
+     * Method parses html page for getting post creation date.
+     *
+     * @param link url of page with post.
+     * @return creation date of post.
+     */
+    public String getCreateDate(String link) {
+        String date = "";
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Element row =
+                    doc.getElementsByClass("msgFooter").get(0);
+            String footer = row.text();
+            date = footer.substring(0, footer.indexOf("[") - 1);
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return date;
     }
 }
